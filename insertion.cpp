1c521ae54f1ed8647a3a2c65cf664fe748cb6944
CYBER773 <ha3cyb3@gmail.com>
2025-09-06 10:56:16 +0530

    typecasting made

--- insertion.cpp
+++ insertion.cpp
@@ -3,10 +3,12 @@ using namespace std;
 int main()
 {
     int a, b;
-    // cin>>a>>b; we can use this that will take value at once
-    cout<<"Enter the value of a: ";
-    cin>>a;
-    cout<<"Enter the value of b: ";
-    cin>>b;
-    cout<<a+b;
+    // // cin>>a>>b; we can use this that will take value at once
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+    // cout<<"Enter the value of b: ";
+    // cin>>b;
+    // cout<<"Sum of a and b is "<<a+b;
+    a =6, b=34;
+    cout<<(a == b);
 }
---
+++ typcasting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=2;
+    char c='b';
+    a=c;
+    cout << a;
+}

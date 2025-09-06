4e42cb6d460eff53c7f2087c8faf755b717d865b
CYBER773 <ha3cyb3@gmail.com>
2025-09-06 15:38:21 +0530

    loop concept

---
+++ loop concept/even.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    for(int a=0; a<=5;a=a+2){
+        cout<<a<<endl;
+    }
+    /* code */
+    int count;
+    cin>>count;
+    /*this code work like as well*/
+    for (int i = 0; i <= count; i++)
+    {
+        if (i%2==0)
+        {
+            cout<<i<<endl;
+        }
+        
+    }
+    
+    
+} // namespace std;
+

d2f17fabb9be125aec00a9180db18e75d437cd0d
CYBER773 <ha3cyb3@gmail.com>
2025-09-06 08:10:07 +0530

    added variables and datatypes

--- first.cpp
+++ first.cpp
@@ -2,6 +2,24 @@
 using namespace std;
 int main()
 {
-    cout<<"this is testing mfs";
-    cout<<"lala";
+    // cout<<"this is testing mfs\n";
+    // cout<<"lala\n";
+    // variable diclaration and we also can do int d = 20;
+//     int i;
+//     i = 20;
+//     // this is character always shown in single quotation
+//     char data ='d';
+//     cout<<data;
+// this is float which is decimal number
+    // float f;
+    // f = 2.1;
+    // cout<<f;
+    // this is boolean
+    bool b, c;
+    b = false;
+    c = 0 ;
+    cout<<b;
+
+
+
 }

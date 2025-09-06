e4f6372114bba0fea8736ccabb4a74e7fd6a02df
CYBER773 <ha3cyb3@gmail.com>
2025-09-06 12:53:47 +0530

    loop

---
+++ loop concept/loop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // for (int count = 0; count <= 5; count++)
+    /*count++ and count=count+1 are work same 
+    count++ mean increment by 1 and count=count+1 also increment by one*/
+    // for (int num=0; num <=6; num = num+1)
+    
+    // {
+    //     cout<<"hello world"<<endl;
+    // }
+    int n;
+    cout<<"Enter the value of N: ";
+    cin>>n;
+    for (int d = 0; d < n; d++)
+    {
+        cout<<"hello world"<<endl;
+    }
+    
+    
+    
+}

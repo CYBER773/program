9c21da692acec9c5ad1071a466472f33df23378a
CYBER773 <ha3cyb3@gmail.com>
2025-09-06 09:04:09 +0530

    insertion of value

---
+++ insertion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    // cin>>a>>b; we can use this that will take value at once
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<a+b;
+}
